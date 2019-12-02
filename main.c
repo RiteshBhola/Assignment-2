4ce187d1615920580fac8e91558f1d89266f51fb
RiteshBhola <57351090+RiteshBhola@users.noreply.github.com>
2019-12-02 08:55:14 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,23 @@
+
+#include"functions.h"
+int main() 
+{ 
+  
+    // This pointer will hold the 
+    // base address of the block created 
+    float* ptr; 
+    int n=100,i;
+  
+   // Dynamically allocate memory using malloc() 
+    ptr = (float*)malloc(n * sizeof(float)); 
+  for (i = 0; i < n; i += 1)
+  {
+    ptr[i]=(i+1)*(i+1);
+  }
+  float res[2];
+  mean_and_variance(ptr,res,n);
+  printf("Mean is %f  and Variance is %f\n",res[0],res[1]);
+  free(ptr);
+  return 0;
+  
+}
